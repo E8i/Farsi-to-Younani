ba35397d3521d59a52fe5c166f0520cdd54e7f02
ebigreece <ebrahim7.hosseinpour@gmail.com>
2021-07-25 12:01:15 +0300

    Farsi to Younani

--- app/src/main/java/come/examp/farsitoyounani/Edame.java
+++ app/src/main/java/come/examp/farsitoyounani/Edame.java
@@ -49,6 +49,79 @@ public class Edame extends AppCompatActivity {
                 startActivity(in);
             }
         });
+        b24=findViewById(R.id.button24);
+        b24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,penao.class);
+                startActivity(in);
+            }
+        });
+        b25=findViewById(R.id.button25);
+        b25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Agorazo.class);
+                startActivity(in);
+            }
+        });
+        b26=findViewById(R.id.button26);
+        b26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Doulevo.class);
+                startActivity(in);
+            }
+        });
+        b27=findViewById(R.id.button27);
+        b27.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Leo.class);
+                startActivity(in);
+            }
+        });
+        b28=findViewById(R.id.button28);
+        b28.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Fonazo.class);
+                startActivity(in);
+            }
+        });
+        b29=findViewById(R.id.button29);
+        b29.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Dipsao.class);
+                startActivity(in);
+            }
+        });
+        b30=findViewById(R.id.button30);
+        b30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Gurizeis.class);
+                startActivity(in);
+            }
+        });
+        b31=findViewById(R.id.button31);
+        b31.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Xtupao.class);
+                startActivity(in);
+            }
+        });
+        b32=findViewById(R.id.button32);
+        b32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame.this,Poulao.class);
+                startActivity(in);
+            }
+        });
+
 
     }
 }
