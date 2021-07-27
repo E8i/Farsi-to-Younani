c92e42aae89846629ca1dc9c99e875b52fa2de65
ebigreece <ebrahim7.hosseinpour@gmail.com>
2021-07-27 20:04:36 +0300

    Farsi to Younani

--- app/src/main/java/come/examp/farsitoyounani/Edame2.java
+++ app/src/main/java/come/examp/farsitoyounani/Edame2.java
@@ -2,13 +2,27 @@ package come.examp.farsitoyounani;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Edame2 extends AppCompatActivity {
+
+    private Button b53;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edame2);
+
+        b53=findViewById(R.id.button53);
+        b53.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in=new Intent(Edame2.this,Edame.class);
+                startActivity(in);
+            }
+        });
     }
 }
